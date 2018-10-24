3f88800229a04c8b0d3f3ba9060c66a0c100bc34
Austinlp4 <apendergrast44@gmail.com>
2018-10-24 17:43:42 -0500

    finished login auth, Signin.js, Signup.js

---
+++ _secrets/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    jwtKey: 'This may or may not be Sparta!',
+  };
--- pallette/src/App.js
+++ pallette/src/App.js
@@ -1,14 +1,58 @@
 import React, { Component } from 'react';
+import { NavLink, Route } from 'react-router-dom';
 import './App.css';
+
+
+import Signin from './auth/Signin';
+import Signup from './auth/Signup';
+
+const Home = props => {
+  return (
+    <div className='container'>
+      <div className='home'>Home</div>
+      <button onClick={props.signout}>Sign out</button>
+    </div>
+  )
+}
 
 class App extends Component {
   render() {
+    if(localStorage.length > 0){
+      return <Home signout={this.signout}/>
+    }
     return (
       <div className="App">
-        
+        <header>
+          <nav>
+            <NavLink to='/' exact>
+               Home
+            </NavLink>
+            
+            <NavLink to='/signin'>
+               Sign in
+            </NavLink>
+           
+            <NavLink to='/signup'>
+               Sign up
+            </NavLink>
+           
+            <button onClick={this.signout}>Sign out</button>
+          </nav>
+          <main>
+            <Route path='/' component={Home} exact></Route>
+            <Route path='/signin' component={Signin}></Route>
+            <Route path='/signup' component={Signup}></Route>
+          </main>
+        </header>
+          
       </div>
     );
   }
+  signout = () => {
+    localStorage.removeItem('jwt');
+    
+  }
 }
+
 
 export default App;
