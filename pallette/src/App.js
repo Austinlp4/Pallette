299cddb4638405cfbb0ee2e3c912fe77c1680169
Austinlp4 <apendergrast44@gmail.com>
2018-11-30 12:25:48 -0600

    styling for landing page done

--- pallette/src/App.js
+++ pallette/src/App.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
-
-import Checkout from './Checkout';
+import styled from 'styled-components';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
+      <Home className="App">
+       <h1>Pallette</h1>
+        <div className="one">
+          <div className="two">
+            <div className="three">
+              
+            </div>
+          </div>
         </div>
-        <p className="App-intro">
-          <Checkout
-            name={'The Road to learn React'}
-            description={'Only the Book'}
-            amount={1}
-          />
-        </p>
-      </div>
+      </Home>
     );
   }
 }
+
+const Home = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding-top: 5px;
+  .one, .two, .three{
+    box-shadow: 0 9px 0px 0px white, 
+    inset 0 -9px 0px 0px white, 
+    inset 10px 0 15px -4px rgba(31, 73, 125, 0.2),
+    inset -10px 0 15px -4px rgba(31, 73, 125, 0.2);
+    display: flex;
+    justify-content: center;
+    width: 90%;
+    height: 800px;
+
+  }
+`;
 
 export default App;
