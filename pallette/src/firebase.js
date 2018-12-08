5d887062d34aad058c4c7fbfcc84b3e1aa315509
Austinlp4 <apendergrast44@gmail.com>
2018-12-08 13:17:11 -0600

    Set up .env

--- pallette/src/firebase.js
+++ pallette/src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyBF005NPJDUpt4536zuBp_FfYbU81Whhu0",
-    authDomain: "pallete-6b871.firebaseapp.com",
-    databaseURL: "https://pallete-6b871.firebaseio.com",
-    projectId: "pallete-6b871",
-    storageBucket: "pallete-6b871.appspot.com",
-    messagingSenderId: "953289263453"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   };
   firebase.initializeApp(config);
   export const provider = new firebase.auth.GoogleAuthProvider();
