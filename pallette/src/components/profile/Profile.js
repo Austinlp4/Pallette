ea529042cc9d26f5db2e09ee3b2f9e0d1f17cbd4
Austinlp4 <apendergrast44@gmail.com>
2018-12-18 21:29:34 -0600

    set up profile

---
+++ pallette/src/components/profile/Profile.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+class Profile extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            
+        };
+    }
+
+    componentDidMount() {
+        
+    }
+
+    render(){
+        
+        return (
+            <div>
+              
+            </div>
+        )
+    }
+}
+
+
+
+
+export default Profile;
