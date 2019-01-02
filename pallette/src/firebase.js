948116fdb8d67d9cfafe8f1d078d3d5b01c0fe04
Austinlp4 <apendergrast44@gmail.com>
2019-01-02 12:55:07 -0600

    fix firebase

--- pallette/src/firebase.js
+++ pallette/src/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/firestore';
 
 
 const config = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    apiKey: "AIzaSyBF005NPJDUpt4536zuBp_FfYbU81Whhu0",
+    authDomain: "pallete-6b871.firebaseapp.com",
+    databaseURL: "https://pallete-6b871.firebaseio.com",
+    projectId: "pallete-6b871",
+    storageBucket: "pallete-6b871.appspot.com",
+    messagingSenderId: "953289263453",
   };
   firebase.initializeApp(config);
   firebase.firestore().settings({ timestampsInSnapshots: true })
