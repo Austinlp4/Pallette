dbb0a1ff218c82f4cf2d24cc2903033d656800d2
Austinlp4 <apendergrast44@gmail.com>
2019-04-19 14:31:15 -0500

    export styles

--- pallette/src/components/featured/MostViewed.js
+++ pallette/src/components/featured/MostViewed.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import styled from 'styled-components';
 import firebase from '../../firebase';
-import AliceCarousel from 'react-alice-carousel';
-import "react-alice-carousel/lib/alice-carousel.css";
+import 'react-alice-carousel/lib/alice-carousel.css';
 import View from '../../images/view.png';
 import Like from '../../images/heart.png';
 import { connect } from 'react-redux';
 import { addView, addLike } from '../../store/actions/projectActions.js';
-import {
-  Card, 
-  Container,
-  Imgcont,
-  Wrapper
-} from './MostViewedStyle';
-
+import { Card, Container, Imgcont, Wrapper } from './MostViewedStyle';
 
 class MostViewed extends React.Component {
   constructor(props) {
@@ -21,42 +13,32 @@ class MostViewed extends React.Component {
 
     this.state = {
       photos: [],
-      
-    }
+    };
   }
 
   responsive = {
     500: { items: 1 },
-        900: { items: 2 },
-        1025: { items: 2 },
-        1300: { items: 2 },
-        1400: { items: 3 },
-        1700: { items: 3 },
-        1800: { items: 4 }
+    900: { items: 2 },
+    1025: { items: 2 },
+    1300: { items: 2 },
+    1400: { items: 3 },
+    1700: { items: 3 },
+    1800: { items: 4 },
   };
 
-  
   componentDidMount() {
     let itemsRef = firebase.database().ref(`photos`);
     itemsRef.on('value', data => {
       let works = [];
       data = Object.values(data.val());
-      data.forEach((child) => {
-        works.push(
-          Object.values(child)
-        );
+      data.forEach(child => {
+        works.push(Object.values(child));
       });
       let newWorks = Object.values(works);
       let photos = [];
-      newWorks.map((child, i) => 
-        child.map((obj, i) => 
-          photos.push(
-            obj
-          )
-        )
-      )
+      newWorks.map((child, i) => child.map((obj, i) => photos.push(obj)));
       let newPhotos = photos.sort((a, b) => a.views - b.views).reverse();
-      console.log(newPhotos)
+      console.log(newPhotos);
       this.setState({
         photos: newPhotos,
       });
@@ -66,99 +48,90 @@ class MostViewed extends React.Component {
   onSlideChange(e) {
     console.log('Item`s position during a change: ', e.item);
     console.log('Slide`s position during a change: ', e.slide);
-  };
+  }
 
   onSlideChanged(e) {
     console.log('Item`s position after changes: ', e.item);
     console.log('Slide`s position after changes: ', e.slide);
-  };
+  }
 
   galleryItems() {
-    return (
-      this.state.photos.map((item, i) => (
-        <div key={`key-${i}`} className="yours-custom-class">
-        <Card
-        id={item.id}
-        key={item.id}
-        post={item}
-      >
-        {/* <NavLink to={`${this.props.match.path}/${post.key}`}> */}
-        <img src={item.url} alt="" onClick={() => this.pageFlip(item.id, item.uid)}/>
-        <div className="banner">
-          <div className="title">
-            <h1>{item.title}</h1>
-            <h4>{item.artist}</h4>
+    return this.state.photos.map((item, i) => (
+      <div key={`key-${i}`} className="yours-custom-class">
+        <Card id={item.id} key={item.id} post={item}>
+          <img
+            src={item.url}
+            alt=""
+            onClick={() => this.pageFlip(item.id, item.uid)}
+          />
+          <div className="banner">
+            <div className="title">
+              <h1>{item.title}</h1>
+              <h4>{item.artist}</h4>
+            </div>
+            <div
+              className="like-container"
+              onClick={() => this.handleLike(item.id, item.uid)}
+            >
+              <img className="like" src={Like} alt="" />
+              <h4>{item.likes}</h4>
+            </div>
+            <div className="view-container">
+              <img className="view" src={View} alt="" />
+              <h4>{item.views}</h4>
+            </div>
           </div>
-          <div className='like-container' onClick={() => this.handleLike(item.id, item.uid)}>
-            <img className="like" src={Like} alt="" />
-            <h4>{item.likes}</h4>
-          </div>
-          <div className='view-container'>
-            <img className="view" src={View} alt="" />
-            <h4>{item.views}</h4>
-          </div>
-        </div>
-
-        {/* </NavLink> */}
-      </Card></div>
-      ))
-    )
-  };
+        </Card>
+      </div>
+    ));
+  }
 
   pageFlip = (key, uid) => {
-    console.log('key, uid', key, uid)
-    this.props.addView(key, uid)
+    console.log('key, uid', key, uid);
+    this.props.addView(key, uid);
     this.props.history.push(`/${uid}/${key}`);
-    
   };
 
   handleLike = (key, uid) => {
     const auth = this.props.auth.uid;
     this.props.addLike(key, uid, auth);
-  }
+  };
 
-
-  render () {
-    // const anObj = this.state.photos[0];
-    // console.log(anObj)
-
+  render() {
     return (
       <Container>
-          {this.state.photos.map((item, i) => (
-      <Card
-      id={item.id}
-      key={item.id}
-      post={item}
-    >
-      {/* <NavLink to={`${this.props.match.path}/${post.key}`}> */}
-      <img src={item.url} alt="" onClick={() => this.pageFlip(item.id, item.uid)}/>
-      <div className="banner">
-        <div className="title">
-          <h1>{item.title}</h1>
-          <h4>{item.artist}</h4>
-        </div>
-        <Imgcont>
-        <div className='like-container' onClick={() => this.handleLike(item.id, item.uid)}>
-          <img className="like" src={Like} alt="" />
-          <h4>{item.likes}</h4>
-        </div>
-        <div className='view-container'>
-          <img className="view" src={View} alt="" />
-          <h4>{item.views}</h4>
-        </div>
-        </Imgcont>
-      </div>
-
-      {/* </NavLink> */}
-    </Card>
-    ))}
+        {this.state.photos.map((item, i) => (
+          <Card id={item.id} key={item.id} post={item}>
+            <img
+              src={item.url}
+              alt=""
+              onClick={() => this.pageFlip(item.id, item.uid)}
+            />
+            <div className="banner">
+              <div className="title">
+                <h1>{item.title}</h1>
+                <h4>{item.artist}</h4>
+              </div>
+              <Imgcont>
+                <div
+                  className="like-container"
+                  onClick={() => this.handleLike(item.id, item.uid)}
+                >
+                  <img className="like" src={Like} alt="" />
+                  <h4>{item.likes}</h4>
+                </div>
+                <div className="view-container">
+                  <img className="view" src={View} alt="" />
+                  <h4>{item.views}</h4>
+                </div>
+              </Imgcont>
+            </div>
+          </Card>
+        ))}
       </Container>
     );
-
   }
-  }
-
-
+}
 
 const mapStateToProps = state => {
   console.log(state);
@@ -168,13 +141,14 @@ const mapStateToProps = state => {
   };
 };
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
     addView: (key, uid) => dispatch(addView(key, uid)),
-    addLike: (key, uid, auth) => dispatch(addLike(key, uid, auth))
-  }
-}
+    addLike: (key, uid, auth) => dispatch(addLike(key, uid, auth)),
+  };
+};
 
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(MostViewed);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MostViewed);
--- pallette/src/components/featured/SingleImage.js
+++ pallette/src/components/featured/SingleImage.js
@@ -1,167 +1,273 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import firebase from '../../firebase';
-import styled from 'styled-components';
-import { addComment} from '../../store/actions/projectActions.js';
+import { addComment } from '../../store/actions/projectActions.js';
 import Avatar from '../../images/avatar-profile.png';
-import { 
-    Settings,
-    CommentBox,
-    Image,
-    Modal,
-    Container,
-    Info
+import {
+  Settings,
+  CommentBox,
+  Image,
+  Modal,
+  Container,
+  Info,
 } from './SingleImageStyle';
 
-class SingleCard extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            post: ''
-        }
-    }
+class SingleCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      post: '',
+    };
+  }
 
-   async componentDidMount(){
-        let itemsRef = firebase.database().ref(`photos/${this.props.match.params.uid}/${this.props.match.params.key}`);
-      await itemsRef.on('value', data => {
-           console.log('data.val', data.val())
-            this.setState({ 
-              post: {
-                  ...data.val()
-            
-            }
-            },() => {
-                this.setState({ 
-                    paletteOne: this.state.post.palette[0],
-                    paletteTwo: this.state.post.palette[1],
-                    paletteThree: this.state.post.palette[2],
-                    paletteFour: this.state.post.palette[3],
-                    paletteFive: this.state.post.palette[4],
-                })
-            })
-        })
-    }
-
-    handleComment = event => {
-        this.setState({
-            [event.target.name]: event.target.value,
+  async componentDidMount() {
+    let itemsRef = firebase
+      .database()
+      .ref(
+        `photos/${this.props.match.params.uid}/${this.props.match.params.key}`
+      );
+    await itemsRef.on('value', data => {
+      console.log('data.val', data.val());
+      this.setState(
+        {
+          post: {
+            ...data.val(),
+          },
+        },
+        () => {
+          this.setState({
+            paletteOne: this.state.post.palette[0],
+            paletteTwo: this.state.post.palette[1],
+            paletteThree: this.state.post.palette[2],
+            paletteFour: this.state.post.palette[3],
+            paletteFive: this.state.post.palette[4],
           });
+        }
+      );
+    });
+  }
+
+  handleComment = event => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const key = this.props.match.params.key;
+    const uid = this.props.match.params.uid;
+    const useruid = this.props.auth.uid;
+    const { comment } = this.state;
+    const url = this.props.profile.url;
+    const name = `${this.props.profile.firstName} ${
+      this.props.profile.lastName
+    }`;
+    this.props.addComment(key, uid, name, url, comment, useruid);
+    this.setState({ comment: '' });
+  };
+
+  closeModal = event => {
+    console.log('uids', event.target.id, this.props.auth.uid);
+    if (event.target.dataset.type === 'modal-container') {
+      if (event.target.id === this.props.auth.uid) {
+        this.props.history.push('/profile');
+      } else {
+        this.props.history.push('/');
+      }
     }
+  };
 
-    handleSubmit = (event) => {
-        event.preventDefault();
-        const key = this.props.match.params.key;
-        const uid = this.props.match.params.uid;
-        const useruid = this.props.auth.uid;
-        const { comment } = this.state;
-        const url = this.props.profile.url;
-        const name = `${this.props.profile.firstName} ${this.props.profile.lastName}`
-        this.props.addComment(key, uid, name, url, comment, useruid)
-        this.setState({ comment: '' })
-    }
+  scrollToBottom = event => {
+    let element = document.getElementById('bottom');
+    element.scrollTop = element.scrollHeight;
+    console.log('height', element.scrollHeight);
+    console.log('top', element.scrollTop);
+  };
 
-    closeModal = (event) => {
-        console.log('uids', event.target.id, this.props.auth.uid)
-        if (event.target.dataset.type === 'modal-container') {
-            if(event.target.id === this.props.auth.uid){
-                this.props.history.push('/profile')
-            } else {
-                this.props.history.push('/')
-            }
-            
-          }
-    }
-
-
-    scrollToBottom = (event) => {
-        // let elementHeight = event.target.scrollHeight;
-        // console.log('scroll top', event.target.scrollTop)
-        // event.target.scrollTop = elementHeight
-        // console.log('scroll top',event.target.scrollTop)
-        let element = document.getElementById('bottom')
-        element.scrollTop = element.scrollHeight;
-        console.log('height', element.scrollHeight)
-        console.log('top', element.scrollTop)
-    }
-
-
-    render(){
-        const post = this.state.post;
-        console.log('uid', post.uid)
-        return (
-            <Container data-type='modal-container' id={post.uid} onClick={this.closeModal}>
-                <Modal>
-                <div style={{ width: '40px', height: '700px' , display: 'flex', flexDirection: 'column'}}>
-                    <div className='uno' style={{ backgroundColor: `${this.state.paletteOne}` , height: '20%', width: '40px'}}></div>
-                    <div className='dos' style={{ backgroundColor: `${this.state.paletteTwo}` , height: '20%', width: '40px'}}></div>
-                    <div className='tres' style={{ backgroundColor: `${this.state.paletteThree}` , height: '20%', width: '40px'}}></div>
-                    <div className='quatro' style={{ backgroundColor: `${this.state.paletteFour}` , height: '20%', width: '40px'}}></div>
-                    <div className='cinco' style={{ backgroundColor: `${this.state.paletteFive}` , height: '20%', width: '40px'}}></div>
+  render() {
+    const post = this.state.post;
+    console.log('uid', post.uid);
+    return (
+      <Container
+        data-type="modal-container"
+        id={post.uid}
+        onClick={this.closeModal}
+      >
+        <Modal>
+          <div
+            style={{
+              width: '40px',
+              height: '700px',
+              display: 'flex',
+              flexDirection: 'column',
+            }}
+          >
+            <div
+              className="uno"
+              style={{
+                backgroundColor: `${this.state.paletteOne}`,
+                height: '20%',
+                width: '40px',
+              }}
+            />
+            <div
+              className="dos"
+              style={{
+                backgroundColor: `${this.state.paletteTwo}`,
+                height: '20%',
+                width: '40px',
+              }}
+            />
+            <div
+              className="tres"
+              style={{
+                backgroundColor: `${this.state.paletteThree}`,
+                height: '20%',
+                width: '40px',
+              }}
+            />
+            <div
+              className="quatro"
+              style={{
+                backgroundColor: `${this.state.paletteFour}`,
+                height: '20%',
+                width: '40px',
+              }}
+            />
+            <div
+              className="cinco"
+              style={{
+                backgroundColor: `${this.state.paletteFive}`,
+                height: '20%',
+                width: '40px',
+              }}
+            />
+          </div>
+          <div className="image-cont">
+            <Image src={post.url} alt="" />{' '}
+          </div>
+          <Info>
+            <h2>{post.title}</h2>
+            <h5>By: {post.artist}</h5>
+            <div>
+              {!post.comments ? (
+                <div>
+                  <h3>Add a comment!</h3>
+                  <div>
+                    <input
+                      type="text"
+                      name="comment"
+                      value={this.state.comment}
+                      onChange={this.handleComment}
+                    />
+                    <button onClick={this.handleSubmit}>Add</button>
+                  </div>
                 </div>
-                    <div className='image-cont'><Image src={post.url} alt="" /> </div>
-                <Info>
-                    <h2>{post.title}</h2>
-                    <h5>By: {post.artist}</h5>
-                    <div>
-                        {!post.comments
-                        ?
-                         <div>
-                            <h3>Add a comment!</h3>
-                            <div>
-                            <input type="text" name='comment' value={this.state.comment} onChange={this.handleComment}/>
-                            <button onClick={this.handleSubmit}>Add</button>
-                        </div>
-                        </div>
-                        :
+              ) : (
+                <div>
+                  <CommentBox
+                    id="bottom"
+                    onLoad={this.scrollToBottom}
+                    className="MessageList"
+                  >
+                    {Object.values(post.comments).map(comment => {
+                      console.log('comment', comment);
+                      return (
                         <div>
-                        <CommentBox id='bottom' onLoad={this.scrollToBottom} className='MessageList'>
-                            {Object.values(post.comments).map(comment => {
-                                console.log('comment', comment)
-                                return (
-                                    <div>
-                                        <div style={{ display: 'flex', width: '180px', justifyContent: 'space-between' , alignItems: 'center'}}>
-                                        {comment.url
-                                        ?
-                                        <img src={comment.url} alt="" style={{ width: '30px', height: '30px' , borderRadius: '50px'}}/>
-                                        :
-                                        <img src={Avatar} alt="" style={{ width: '30px', height: '30px' , borderRadius: '50px'}}/>
-                                    }
-                                    
-                                    <h3 style={{ fontSize: '1.1rem' , fontWeight: '300'}}>{comment.name}</h3>
-                                    </div>
-                                    <p style={{ margin: '2px auto', color: 'white' }}>{comment.comment}</p>
-                                </div>
-                                )
-                            })}
-                        </CommentBox>
-                        <div style={{ display: 'flex', alignItems: 'center' , marginTop: '10px'}}>
-                            <textarea type="text" name='comment' value={this.state.comment} placeholder='Add a Comment...' onChange={this.handleComment} style={{ padding:'5px', width: '300px', height: '75px', borderRadius: '6px', border: 'none',backgroundColor: 'rgb(28, 40, 50)', color: 'white' }}/>
-                            <Settings onClick={this.handleSubmit}>Add</Settings>
+                          <div
+                            style={{
+                              display: 'flex',
+                              width: '180px',
+                              justifyContent: 'space-between',
+                              alignItems: 'center',
+                            }}
+                          >
+                            {comment.url ? (
+                              <img
+                                src={comment.url}
+                                alt=""
+                                style={{
+                                  width: '30px',
+                                  height: '30px',
+                                  borderRadius: '50px',
+                                }}
+                              />
+                            ) : (
+                              <img
+                                src={Avatar}
+                                alt=""
+                                style={{
+                                  width: '30px',
+                                  height: '30px',
+                                  borderRadius: '50px',
+                                }}
+                              />
+                            )}
+
+                            <h3
+                              style={{ fontSize: '1.1rem', fontWeight: '300' }}
+                            >
+                              {comment.name}
+                            </h3>
+                          </div>
+                          <p style={{ margin: '2px auto', color: 'white' }}>
+                            {comment.comment}
+                          </p>
                         </div>
-                        </div>
-                    }
-                    </div>
-                </Info>
-                </Modal>
-            </Container>
-        )
-        
-    }
+                      );
+                    })}
+                  </CommentBox>
+                  <div
+                    style={{
+                      display: 'flex',
+                      alignItems: 'center',
+                      marginTop: '10px',
+                    }}
+                  >
+                    <textarea
+                      type="text"
+                      name="comment"
+                      value={this.state.comment}
+                      placeholder="Add a Comment..."
+                      onChange={this.handleComment}
+                      style={{
+                        padding: '5px',
+                        width: '300px',
+                        height: '75px',
+                        borderRadius: '6px',
+                        border: 'none',
+                        backgroundColor: 'rgb(28, 40, 50)',
+                        color: 'white',
+                      }}
+                    />
+                    <Settings onClick={this.handleSubmit}>Add</Settings>
+                  </div>
+                </div>
+              )}
+            </div>
+          </Info>
+        </Modal>
+      </Container>
+    );
+  }
 }
 
+const mapDispatchToProps = dispatch => {
+  return {
+    addComment: (key, uid, name, url, comment, useruid) =>
+      dispatch(addComment(key, uid, name, url, comment, useruid)),
+  };
+};
 
+const mapStateToProps = state => {
+  return {
+    auth: state.firebase.auth,
+    profile: state.firebase.profile,
+  };
+};
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        addComment: (key, uid, name, url, comment, useruid) => dispatch(addComment(key, uid, name, url, comment, useruid)),
-    }
-}
-
-const mapStateToProps = (state) => {
-    return {
-        auth: state.firebase.auth,
-        profile: state.firebase.profile
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(SingleCard);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SingleCard);
